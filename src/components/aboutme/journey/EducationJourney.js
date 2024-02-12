93c587ea328aa6caae3ece1de1ed69f6d777a5ff
Girish More <girishmore097@gmail.com>
2024-02-12 09:17:44 +0530

    first

---
+++ src/components/aboutme/journey/EducationJourney.js
@@ -0,0 +1,99 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import SchoolIcon from "@mui/icons-material/School";
+import { Link } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
+import Flip from "react-reveal/Flip";
+
+function EducationJourney() {
+  return (
+    <div className="mt-5">
+      <div>
+        <Link className="btn btn-primary m-1" to="/educationjourney">
+          Educational Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/experiencejourney">
+          Experience Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/technicaljourney">
+          Technical Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/projectjourney">
+          Project Journey
+        </Link>
+      </div>
+      <div className="mt-5">
+        <Flip top cascade>
+          <h1>Qualification Journey</h1>
+        </Flip>
+      </div>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2010 - 2020"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">
+            SECONDARY SCHOOL CERTIFICATE
+          </h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            S.H.G.S High School, Nandurbar.
+          </h4>
+          <p>
+            Studied here from Class 1 to 10th as a part of secondary school
+            Certificate and gain 91.6 % in my SSC Examination.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2020 - 2022"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">
+            HIGHER SECONDARY SCHOOL CERTIFICATE
+          </h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            S.A Mission Junior College, Nandurbar.
+          </h4>
+          <p>
+            Studied here for class 11th and 12th in Science field 
+            and gain 85.83 %.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2022 - 2026"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">GRADUATION</h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            Visvesvaraya National Institute of Technology, Nagpur.
+          </h4>
+          <p>
+            Studied Bachelor of Technology degree in Metallurgical and Material
+            Engineering and gain CGPA 7.12 upto 3 semester. 
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          iconStyle={{ background: "#fbd9ad", color: "rgb(78 22 112)" }}
+          icon={<StarIcon />}
+        />
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default EducationJourney;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
+import { FaLinkedinIn,FaGoogle } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Girish More</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} Girish More</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/girishmore2004"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/girish-more-085b9924a/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/girishmore70/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="mailto:girishmore097@gmail.com/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGoogle />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
