8cf981a90cb96b07231785d5dc15760a6917030c
Girish More <girishmore097@gmail.com>
2024-05-27 10:36:11 +0530

    first commit

--- backend/routes/Contact.js
+++ backend/routes/Contact.js
@@ -1,35 +1,48 @@
-require('dotenv').config();
-const express = require('express');
-const twilio = require('twilio');
-const Contact = require('../models/Contact');
-const router = express.Router();
- 
-const accountSid =process.env.TWILIO_ACCOUNT_SID;
-const authToken =process.env.TWILIO_AUTH_TOKEN;
-const fromPhoneNumber =process.env.TWILIO_FROM_PHONE_NUMBER;
-const toPhoneNumber =process.env.TWILIO_TO_PHONE_NUMBER;
+import dotenv from 'dotenv';
+dotenv.config();
 
- 
-const client = new twilio(accountSid, authToken);
+import express from 'express';
+import twilio from 'twilio';
+import Contact from '../models/Contact';
+
+const router = express.Router();
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_FROM_PHONE_NUMBER, TWILIO_TO_PHONE_NUMBER } = process.env;
+
+if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN || !TWILIO_FROM_PHONE_NUMBER || !TWILIO_TO_PHONE_NUMBER) {
+    console.error("Twilio environment variables are not properly configured.");
+    process.exit(1);
+}
+
+const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+const sendMessage = async (name, message) => {
+    try {
+        const twilioResponse = await client.messages.create({
+            body: `New submission from ${name}: ${message}`,
+            from: TWILIO_FROM_PHONE_NUMBER,
+            to: TWILIO_TO_PHONE_NUMBER,
+        });
+        console.log('Twilio response:', twilioResponse);
+        return twilioResponse;
+    } catch (error) {
+        console.error('Error occurred while sending message:', error);
+        throw error;
+    }
+};
 
 router.post('/', async (req, res) => {
     const { name, email, message } = req.body;
 
-    try { 
+    try {
         if (!name || !email || !message) {
             return res.status(400).json({ message: 'Name, email, and message are required.' });
         }
- 
+
         const newContact = new Contact({ name, email, message });
- 
         await newContact.save();
- 
-        const twilioResponse = await client.messages.create({
-            body: `New submission from ${name}: ${message}`,
-            from: fromPhoneNumber,
-            to: toPhoneNumber,
-        }); 
-        console.log('Twilio response:', twilioResponse); 
+
+        await sendMessage(name, message);
+
         res.status(201).json({ message: 'Contact submitted successfully', contact: newContact });
     } catch (error) {
         console.error('Error occurred while submitting contact:', error);
@@ -37,4 +50,4 @@ router.post('/', async (req, res) => {
     }
 });
 
-module.exports = router;
+export default router;
