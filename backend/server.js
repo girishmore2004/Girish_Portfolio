7c87ffa459fb8136951895372982466164f6d4df
Girish More <girishmore097@gmail.com>
2024-05-30 11:47:15 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ app.use(cors());  // Enable CORS for all routes
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/ContactForm', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
