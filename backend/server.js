532351054a4815427912018808f6efef39115118
Girish More <girishmore097@gmail.com>
2024-08-07 18:42:45 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
